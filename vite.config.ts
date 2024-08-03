aa9ab55d7c3b37d6fec9a419bf872b127ea4b1f6
fsanntiago <zerofabricio@hotmail.com>
2024-08-03 16:12:54 -0300

    chore: add tailwind and shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { crx } from '@crxjs/vite-plugin';
-import manifest from './src/manifest';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./src/manifest";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+    hmr: { protocol: "ws", host: "localhost", port: 3000 },
+  },
   build: {
     emptyOutDir: true,
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       output: {
-        chunkFileNames: 'assets/chunk-[hash].js',
+        chunkFileNames: "assets/chunk-[hash].js",
       },
     },
   },
   plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
